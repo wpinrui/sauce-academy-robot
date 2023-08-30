7131cd85c64f14e4b34ed9c202b00bfe3478199f
Ivan <77185900+wpinrui@users.noreply.github.com>
2023-08-31 02:39:42 +0800

    Update help.js

--- src/features/help/help.js
+++ src/features/help/help.js
@@ -28,6 +28,6 @@ export default class Help {
     await bot.sendMessage(msg.chat.id, '2. The lecture resources: https://canvas.nus.edu.sg/courses/45621/files/folder/Lecture%20and%20Brief%20Slides');
     await bot.sendMessage(msg.chat.id, '3. The Reflection Google Drive: https://drive.google.com/drive/folders/1STwlPtSjbQUi_IS0NrPLil3CFXUEaZHb?usp=sharing');
     await bot.sendMessage(msg.chat.id, '4. Ask your avenger or reflection tutor for help! Try the "consult" keyword.');
-    await bot.sendMessage(msg.chat.id, "5. Last but not least, if you don't need a consultation and just want a quick explanation, do not hesitate to ask me directly :) Either use the keyword \"askivan\", or just message @wpinrui directly.");
+    await bot.sendMessage(msg.chat.id, "5. Last but not least, if you don't need a consultation and just want a quick explanation, do not hesitate to ask me @wpinrui directly :)");
   };
 }
