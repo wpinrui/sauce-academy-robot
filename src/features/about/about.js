1938fbd7011d1503bb3db3d7c9f33218ba33cd33
Ivan <77185900+wpinrui@users.noreply.github.com>
2023-08-30 21:35:03 +0800

    Update about.js

--- src/features/about/about.js
+++ src/features/about/about.js
@@ -5,10 +5,27 @@ export default class About {
     'what are you': 2,
     'what is this': 2,
     'abt': 1,
+    'commands': 5,
+    'command': 5,
   };
 
   static handler = async (bot, msg) => {
     await bot.sendMessage(msg.chat.id, 'Interpreting as "about"');
     await bot.sendMessage(msg.chat.id, 'This bot is created by Ivan (https://github.com/wpinrui) and is not officially affiliated with CS1101S.');
+    await bot.sendMessage(msg.chat.id, 'You can use the following commands:');
+    const commnadString = `/start - Say hi to SauceBot!
+/about - About SauceBot (and its Saucecode)
+/consult - [COMING SOON] Book a consult
+/feedback - Tell me how to improve the reflections!
+/hello - Say hi to SauceBot... again!
+/help - Get the help you need
+/join - Join the Telegram group
+/recording - Rewatch the lectures
+/schedule - See what's due soon
+/slides - Download the reflection slides
+/suggest - Suggest a feature for SauceBot
+/zoom - Get the lecture link
+/chat - Like chatgpt but less good`;
+    await bot.sendMessage(msg.chat.id, commnadString);
   };
 }
